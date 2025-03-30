99abf298164492d3bcc9ecaa59f0ea313bd63b6f
Rafael de Lima <rafalimabcl@gmail.com>
2025-03-30 12:23:27 -0300

    feat: :tada: initial commit

---
+++ src/main/java/com/hacksociety/techtrack/TechtrackApplication.java
@@ -0,0 +1,13 @@
+package com.hacksociety.techtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacksociety/techtrack/TechtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacksociety.techtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
