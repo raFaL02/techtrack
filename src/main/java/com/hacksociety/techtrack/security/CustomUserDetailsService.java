bb200a6046dba8bc20b6be894aa29b80ab60cf80
Rafael de Lima <rafalimabcl@gmail.com>
2025-04-08 15:44:22 -0300

    feat(auth): add custom user details service.

---
+++ src/main/java/com/hacksociety/techtrack/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.hacksociety.techtrack.security;
+
+import com.hacksociety.techtrack.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.hacksociety.techtrack.models.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
